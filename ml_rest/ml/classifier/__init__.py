c3b485baa11b4b548b3d99e0de04bebc6d8b79d2
Dell <jaehyun0803@naver.com>
2019-05-22 16:07:15 +0900

    알고리즘 추가 1. linearsvc

--- ml_rest/ml/classifier/__init__.py
+++ ml_rest/ml/classifier/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['bagging', 'bernoulinb','randomforest','decisiontree','extratrees','extratree','gaussiannb','gaussianprocess','svc','mlp','kneighbors']
+__all__ = ['bagging', 'bernoulinb','randomforest','decisiontree','extratrees','extratree','gaussiannb','gaussianprocess','svc','mlp','kneighbors','linearsvc']
