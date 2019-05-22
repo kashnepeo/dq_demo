149d02e250ec5892d8cabbc7443af695aa31dcfe
Dell <jaehyun0803@naver.com>
2019-05-22 09:54:22 +0900

    알고리즘 추가 1. gaussiannb 2. gaussianprocessclassfier

--- ml_rest/ml/classifier/__init__.py
+++ ml_rest/ml/classifier/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['bagging', 'bernoulinb','randomforest','decisiontree','extratrees','extratree']
+__all__ = ['bagging', 'bernoulinb','randomforest','decisiontree','extratrees','extratree','gaussiannb','gaussianprocess']
