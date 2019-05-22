2e17a3c798b643940ef0d1b789019aed59dfbbdf
Eunki7 <outsider7224@hanmail.net>
2019-05-22 19:59:47 +0900

    init

--- ml_rest/ml/classifier/__init__.py
+++ ml_rest/ml/classifier/__init__.py
@@ -1,1 +1,3 @@
-__all__ = ['bagging', 'bernoulinb', 'randomforest', 'decisiontree', 'extratrees', 'extratree', 'gaussiannb', 'gaussianprocess', 'svc', 'mlp', 'kneighbors', 'linearsvc', 'logisticregression', 'nusvc','perceptron']
+__all__ = ['bagging', 'bernoulinb', 'randomforest', 'decisiontree', 'extratrees', 'extratree', 'gaussiannb',
+           'gaussianprocess', 'svc', 'mlp', 'kneighbors', 'linearsvc', 'logisticregression', 'nusvc', 'perceptron',
+           'gradientboosting', 'sgd']
