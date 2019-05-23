949341552ed0825b0f83856fbc979c63e39ade4b
Eunki7 <outsider7224@hanmail.net>
2019-05-23 20:35:00 +0900

    regression 작성 중

--- ml_rest/ml/regression/__init__.py
+++ ml_rest/ml/regression/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ['ard_rg', 'bagging_rg', 'linear_rg', 'svr_rg', 'decisiontree_rg', 'cca_rg', 'extratree_rg', 'extratrees_rg',
-           'gaussianprocess_rg', 'gradientboosting_rg', 'kernelridge_rg']
+           'gaussianprocess_rg', 'gradientboosting_rg', 'kernelridge_rg', 'lars_rg', 'larscv_rg', 'lasso_rg', 'lassocv_rg']
